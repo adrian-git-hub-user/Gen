7725a3669c186dd1eb9edbdd0322cb54492126d4
Adrian F <adrian.ronayne@gmail.com>
2015-08-09 23:02:55 +0100

    update

--- src/clean/ValidateData.scala
+++ src/clean/ValidateData.scala
@@ -5,18 +5,17 @@ class ValidateData(numberWordsPerLine: Int, lines: List[String]) {
   def getValidData = {
 
     val charsToClean: List[String] = List("\"", "[", "]", "'")
+    val charsToClean1: Set[Char] = Set('\"', '[', ']', ''')
 
     val trimmedLines = lines.map { m => m.trim() }
     val validLines = trimmedLines.filter(line => line.split("\\s+").size == numberWordsPerLine)
     val invalidLines = trimmedLines.filter(line => line.split("\\s+").size == numberWordsPerLine)
 
-    val filtered = validLines.map(line => line
-      .replace(charsToClean(0), "")
-      .replace(charsToClean(1), "")
-      .replace(charsToClean(2), "")
-      .replace(charsToClean(3), ""))
+    val filtered = validLines.map(line => line.filterNot(c => charsToClean.contains(c)))
 
-    (filtered, invalidLines)
+    val filtered1 = validLines.map(line => line.filterNot(c => charsToClean1.contains(c)))
+
+    (filtered1, invalidLines)
 
   }
 }
